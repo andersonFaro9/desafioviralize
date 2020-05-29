7d043d4d97d1cc7f25624021234e75bceaa507fb
Anderson <faro.anderson@gmail.com>
2020-05-29 14:46:06 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- aqui -->
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          <template>
+
+            <div class="row justify-center">
+              <div class="col-auto">
+                <ul class="list-menu">
+                  <img src="~assets/monday-logo.png" style="width:154px">
+                    <li class="q-ml-lg"><a href = "#">Produto</a> </li>
+                    <li  class="q-ml-lg"><a href = "#"> Histórias de uso</a></li>
+                    <li  class="q-ml-lg"><a href = "#">Trabalho remoto</a></li>
+                    <li class="q-ml-lg"><a href = "#">Corporativo</a></li>
+                    <li class="q-ml-lg"><a href = "#">Preços</a></li>
+                      <li class="q-ml-xl">
+                        <a class="q-ml-xl" href="#"> Contate a equipe de vendas</a>
+                      </li>
+                      <li class="q-ml-xl">
+                         <a  href="#"> Login</a>
+                      </li>
+                    <q-btn  class="q-ml-lg" no-caps rounded color="pink-12" size="md" label="Comece já >"/>
+                </ul>
+
+              </div>
+
+            </div>
+           </template>
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  components: {
+    // EssentialLink
+  }
+}
+</script>
+<style lang="css" scoped>
+.list-menu {
+  color: black;
+  list-style-type:none;
+  display: flex;
+  align-items: center;
+  font-size: 17px;
+  font-weight: 400;
+
+}
+.list-menu a {
+  text-decoration: none;
+  }
+
+ a {
+  text-decoration: none;
+  color: rgb(66, 67, 69);
+
+}
+
+.list-menu a:hover {
+  color:#4d7fed;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,478 @@
+/* eslint-disable no-trailing-spaces */
+<template>
+  <q-page>
+    <div class="mainlayout">
+      <div class="row justify-center q-pa-md">
+        <div class="col-auto text-h3 text-bold" style="max-width: 750px">
+          <span class="text-h2 text-green text-bold">Juntos. </span>
+          Em projetos, processos e nos êxitos. Onde você estiver.
+        </div>
+      </div>
+
+      <div class="row justify-center">
+        <div class="col-auto text-body1">
+          A monday.com é a Work OS que potencializa o trabalho remoto em equipe.
+        </div>
+      </div>
+
+      <div class="row justify-center q-pa-lg">
+        <div class="col-auto">
+          <q-btn no-caps rounded color="pink-12" size="lg" label="Comece já >"/>
+        </div>
+      </div>
+
+      <div class="row justify-center">
+        <div class="col-auto">
+         <div class="q-pa-md">
+          <div class="row items-start">
+            <div class="col-6 ">
+              <q-img
+                src="~assets/globe_top_people.jpg"
+                style="width: 1200px">
+                    <div class=" bg-transparent large-text absolute-center word-spacing-text text-black text-h4 text-center">
+                       Mais de 100.000 organizações confiam na monday.com
+                    </div>
+
+                    <div class=" margin-top-center absolute-center bg-transparent">
+                      <ul class=" list-menu">
+                        <li class="q-ml-xl "><img src="~assets/unilever.png" ></li>
+                        <li  class="q-ml-xl"><img src="~assets/magazineluiza.png" ></li>
+                        <li  class="q-ml-xl"><img  src="~assets/coca_cola.png" ></li>
+                        <li  class="q-ml-xl"><img src="~assets/omelete.png" ></li>
+                        <li  class="q-ml-xl"><img src="~assets/globoplay.png" ></li>
+                        <li  class="q-ml-xl"><img src="~assets/bosch.png" ></li>
+                        <li  class="q-ml-xl"><img src="~assets/apple.png" ></li>
+                        <li  class="q-ml-xl"><img src="~assets/visa.png" ></li>
+                      </ul>
+                    </div>
+                </q-img>
+
+            </div>
+
+            </div>
+
+            </div>
+
+        </div>
+
+      </div>
+      <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+          <div class="">
+            <video width="640" height="480" autoplay loop>
+              <source src="~assets/Gray_ProjectPlan.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 ">
+          <div class="text-h3 q-px-xl">
+           Trabalhe em uma plataforma que se adapta às suas necessidades.
+           <span class="text-green">.</span>
+          </div>
+          <div class="col-auto text-h5 text-grey text-justify q-px-xl q-mt-xl">
+           Com a monday.com podemos trabalhar de uma maneira que se adequa às nossas necessidades e mantém as equipes alinhadas e empoderadas.
+          </div>
+          <div class="col-auto q-px-xl q-mt-xl">
+            <q-list borered padding>
+              <q-item>
+                <q-item-section top avatar>
+                  <img src="~assets/mark_HubSpot.png" width="60" height="60">
+                </q-item-section>
+
+                <q-item-section>
+                  <q-item-label class="text-bold">Mark Znutas</q-item-label>
+                  <q-item-label caption>Diretor de Operações Sênior, HubSpot</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+        </div>
+
+      </div>
+
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6  ">
+
+              <div class="text-h3 q-px-xl q-px-xl margin-top-center2 ">
+                Livre sua equipe de trabalhos manuais chatos e repetitivos<span class="text-green">.</span>
+              </div>
+
+            <div class="text-h5 text-grey q-px-xl q-mt-xl">
+              Nós automatizamos muitos de nossos processos cotidianos.
+              Agora podemos dedicar mais tempo aos trabalhos que requerem nosso talento.
+            </div>
+            <div class="col-auto q-px-xl q-mt-xl">
+              <q-list borered padding>
+                <q-item>
+                  <q-item-section  top avatar>
+                    <img  src="~assets/Samyuktha.jpg" width="60" height="60">
+                  </q-item-section>
+
+                  <q-item-section>
+                    <q-item-label class="text-bold">Samyuktha Shivakumar</q-item-label>
+                    <q-item-label caption>Chefe de Operações para Marketing Global, ThoughtWorks</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="flex-break q-py-md"></div>
+            <div class="flex-break q-py-md"></div>
+
+            <div class="flex-break q-py-md">
+              <video width="640" height="480" autoplay loop>
+                <source src="~assets/Gray_SPipline.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
+            </div>
+
+          </div>
+
+        </div>
+
+          <div class="row  justify-center flex-break q-py-md ">
+            <div class=" text-h4  ">
+              <p>Integre com suas ferramentas existentes</p>
+               <p class="row justify-center"> e realize muito mais! </p>
+             </div>
+
+             <img  src="~assets/imagens.png" width="1889" height="345">
+
+        </div>
+            <div class="text-blue row  justify-center text-weight-bold">
+              <a href="https://monday.com/lang/pt/integrations"> Veja todas as integrações > </a> </div>
+            </div>
+
+        <div class="row justify-center flex-break q-py-xl">
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+          <div class="row justify-center  ">
+            <video width="640" height="480" autoplay loop>
+              <source src="~assets/vv.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+        <div class=" col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 ">
+          <div class="flex-break q-py-md"> </div>
+          <div class="flex-break q-py-md"> </div>
+          <div class="text-h2 q-px-xl justify-center">
+           Colabore em um espaço de trabalho compartilhado
+           <span class="text-yellow-9">.</span>
+          </div>
+          <div class="col-auto text-h5 text-black text-left q-px-xl q-mt-xl">
+           Ter um centro de trabalho virtual para nossa empresa nos permite ter comunicação e colaboração em qualquer lugar do mundo, seja em terra ou no mar.
+          </div>
+          <div class="col-auto q-px-xl q-mt-xl">
+            <q-list borered padding>
+              <q-item>
+                <q-item-section top avatar>
+                  <img src="~assets/Richard Brilliant.png" width="60" height="60">
+                </q-item-section>
+
+                <q-item-section>
+                  <q-item-label class="text-bold">Richard Brilliant</q-item-label>
+                  <q-item-label caption>SVP e Diretor de Auditoria, Carnival Corporation & PLC</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6  ">
+
+              <div class="text-h2 text-left q-px-xl  margin-top-center2 ">
+                Ganhe uma visão completa da situação<span class="text-green">.</span>
+              </div>
+
+            <div class=" text-h5 text-grey q-px-xl q-mt-xl">
+              A monday.com nos deu a visibilidade da qual precisamos para termos todos na mesma página e para acompanhar todas as coisas em andamento.
+            </div>
+            <div class="col-auto q-px-xl q-mt-xl">
+              <q-list borered padding>
+                <q-item>
+                  <q-item-section  top avatar>
+                    <img  src="~assets/jason.png" width="60" height="60">
+                  </q-item-section>
+
+                  <q-item-section>
+                    <q-item-label class="text-bold">Jason Doan</q-item-label>
+                    <q-item-label caption>VP de Heavy Rental e Operações de Vendas, HOLT CAT</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <div class="flex-break q-py-md"></div>
+            <div class="flex-break q-py-md"></div>
+
+            <div class="flex-break q-py-md">
+              <video width="640" height="480" autoplay loop>
+                <source src="~assets/ww.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+              </video>
+            </div>
+
+          </div>
+
+        </div>
+
+           <div class="row justifiy-center text-line ">
+              <div class="text-h2 text-left" >
+               monday.com
+              <div>para trabalho remoto</div>
+              <div class=" text-weight-light text-line text-h5 q-py-md">Una sua equipe a qualquer lugar e em qualquer momento,
+                <div class="text-weight-light"> para colaborar, gerenciar e monitorar seu trabalho</div>
+              </div>
+               <q-btn no-caps rounded color="pink-12" size="q-pa-lg" label="Saiba mais >"/>
+           </div>
+
+           <q-img
+                src="~assets/pc.png"
+                style="width: 650px">
+          </q-img>
+          </div>
+          <div class="text-h2 row justify-center"> Comece sua avaliação de 14 dias  <span class=" q-pl-lg text-green">  gratuita.</span></div>
+          <div class="row justify-center q-pa-lg">
+          <div class="col-auto">
+            <q-btn no-caps rounded color="pink-12" size="lg" label="Comece já   >"/>
+          </div>
+      </div>
+       <div class="row justify-center col-auto q-px-xl q-mt-xl">
+             <q-list borered padding>
+                <q-item>
+                  <q-item-section  top avatar>
+                    <img  src="~assets/monday-logo.png">
+                    <q-item-label class="  q-mr-xl text-left" caption>Todos os direitos reservados © monday.com</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-item>
+                  <q-item-section  top avatar>
+                    <q-item-label class="text-weight-bolder  text-left" caption>Endereço: </q-item-label>
+                    <q-item-label class="text-left  q-mt-md q-pt-sm" caption>26 W 17th St. New York, NY 10011, USA </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item>
+                  <q-item-section  top avatar>
+                    <q-item-label class="text-weight-bolder text-left" caption>
+                      <a href="#"><img  src="~assets/play.png"> </a>
+                      <a href="#"><img class="q-pl-lg" src="~assets/apple-store-button.png"></a>
+                      </q-item-label>
+
+                  </q-item-section>
+                 </q-item>
+                </q-list>
+
+                <q-list borered padding>
+                <q-item>
+                  <q-item-section>
+                      <div class="line-h-list-bottom">
+                          <q-item-label class=" q-mr-xl text-left" caption>
+                              <p class="text-bold">Produto</p>
+                              <div><a class="q--xl" href="#">Produto </a></div>
+                              <div> <a href="#">  Preços </a></div>
+                              <div><a href="#">Corporativo </a></div>
+                              <div> <a href="#">  Parceiros </a></div>
+                              <div><a href="#">Afiliados </a></div>
+                              <div> <a href="#">  Integrações </a></div>
+                              <div><a href="#">monday Apps Framework </a></div>
+                              <div> <a href="#">Desenvolvedores</a></div>
+                              <div><a href="#">Estudantes</a></div>
+                              <div> <a href="#">  Work OS </a></div>
+                          </q-item-label>
+                      </div>
+                  </q-item-section>
+                </q-item>
+
+                </q-list>
+
+                <q-list class="" borered padding>
+                <q-item>
+                  <q-item-section>
+                      <div class="line-h-list-bottom">
+                        <q-item-label class="   text-left" caption>
+                            <p class="text-bold">Equipe</p>
+                            <div><a href="#">Quem somos </a></div>
+                            <div> <a href="#">  Contate-nos </a></div>
+                            <div><a href="#">Carreiras</a></div>
+                            <div> <a href="#">  Encontre um parceiro </a></div>
+                        </q-item-label>
+                        </div>
+                  </q-item-section>
+                </q-item>
+
+                </q-list>
+
+                <q-list borered padding>
+                <q-item>
+                  <q-item-section>
+                      <div class="line-h-list-bottom">
+                          <q-item-label class=" q-ml-xl  text-left" caption>
+                              <p class="text-bold">Histórias sobre uso</p>
+                              <div><a href="#">Remote Work </a></div>
+                              <div> <a href="#">  Gestão de projetos</a></div>
+                              <div><a href="#">Marketing</a></div>
+                              <div> <a href="#">  Desenvolvimento de Software </a></div>
+                              <div><a href="#">Operações e Ti</a></div>
+                              <div> <a href="#">  RH </a></div>
+                              <div><a href="#">Vendas </a></div>
+                              <div> <a href="#">Mídia e produção</a></div>
+                              <div><a href="#">Todos os casos de uso  </a></div>
+
+                          </q-item-label>
+                      </div>
+                  </q-item-section>
+                </q-item>
+               </q-list>
+               <q-list borered padding>
+                <q-item>
+                  <q-item-section>
+                      <div class="line-h-list-bottom">
+                        <q-item-label class=" q-ml-xl text-left" caption>
+                            <p class="text-bold">Recursos</p>
+                            <div><a href="#">Centro de Ajuda </a></div>
+                            <div> <a href="#">  Guias</a></div>
+                            <div><a href="#">Webinars diários</a></div>
+                            <div> <a href="#">  Comunidade</a></div>
+                            <div><a href="#">Depoimentos</a></div>
+                            <div> <a href="#">  Templates</a></div>
+                            <div><a href="#">Serviços Profissionais</a></div>
+                            <div> <a href="#">Tutoriais em vídeo</a></div>
+                            <div><a href="#">Blog</a></div>
+                            <div><a href="#">Podcast</a></div>
+
+                        </q-item-label>
+                    </div>
+                  </q-item-section>
+                </q-item>
+               </q-list>
+
+            </div>
+            <hr class="hr-line">
+          <ul class="list-menu">
+
+                    <li class="q-ml-sm"><a href = "#">Status</a> </li>
+                    <li  class="q-ml-sm"><a href = "#"> Segurança</a></li>
+                     <li  class="q-ml-sm"> <img src="~assets/iso.png" style="width:22px"></li>
+                     <li  class="q-ml-sm"> <img src="~assets/gdpr.png" style="width:22px"></li>
+                     <li  class="q-ml-sm"> <img src="~assets/soc.png" style="width:22px"></li>
+                     <li  class="q-ml-sm"> <img src="~assets/hipaa.png" style="width:22px"></li>
+                    <li class="q-ml-sm"><a href = "#">Termos de serviço</a></li>
+                    <li class="q-ml-sm"><a href = "#">Nossa política de privacidade</a></li>
+
+                        <a class="q-ml-sm" href="#"> <img src="~assets/soc.png" style="width:22px"> Português</a>
+
+                         <li class="q-ml-xl">
+                           <a class=" link-bottom q-ml-xl"  href="#">
+                           <img src="~assets/tww.png" style="width:22px"></a>
+                          </li>
+
+                         <li class="q-ml-sm">
+                           <a class="q-ml-sm" href="#">
+                             <img src="~assets/youtube.png" style="width:22px">
+                           </a>
+                          </li>
+                          <li class="q-ml-sm">
+                            <a  href="#"> <img src="~assets/linkedin.png" style="width:22px"></a>
+                          </li>
+                          <li class="q-ml-sm">
+                            <a  href="#"> <img src="~assets/facebook.png" style="width:22px"></a>
+                          </li>
+                          <li class="q-ml-sm">
+                            <a  href="#"> <img src="~assets/instagram.png" style="width:22px"></a>
+                          </li>
+
+          </ul>
+     </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style lang="css" scoped>
+
+.text-h2{
+   font-size: 40px;
+    line-height: 48px;
+    font-weight: 400;
+}
+.hr-line {
+  width: 1160px;
+}
+.text-h3 {
+    font-size: 20px;
+    line-height: 32px;
+    display: flexbox;
+    flex-wrap: wrap;
+}
+
+.text-line {
+
+  justify-content: center;
+  align-items: center;
+}
+.mainlayout {
+  margin: 0 230px;
+  color: #383B45;
+}
+.word-spacing-text {
+  word-spacing: 7px;
+}
+
+.large-text {
+  width: 1150px;
+}
+.margin-top-center {
+  padding-top: 250px;
+}
+
+.margin-top-center2 {
+  padding-top: 80px;
+}
+
+.list-menu {
+  color: black;
+  list-style-type:none;
+  display: flex;
+  border-top-color: 1px solid blue;
+  justify-content: center;
+  margin-top: 30px;
+  align-items: center;
+  }
+.line-h-list-bottom a {
+  color: rgb(104, 104, 104);
+  line-height: 2.9em;
+}
+
+.link-bottom a {
+  color: #383B45;
+}
+.link-bottom a:link {
+  color: #383B45;
+}
+a {
+  text-decoration: none;
+  color:#87a6e9;
+
+}
+
+a:hover {
+  color:#4d7fed;
+}
+ .margin-text-right {
+   padding-right: 12px;
+   top: 0px;
+   line-height: 1.2em;
+   text-align: right;
+ }
+
+</style>
